063e969a35014b15c8673b2ba94ce1a994466634
Mohamed Sihan <39284618+Mshihan@users.noreply.github.com>
2021-09-13 01:38:49 +0530

    Added Modal to app

--- src/Component/Modal/Modal.js
+++ src/Component/Modal/Modal.js
@@ -11,11 +11,30 @@ const Modal = (props) => {
       ></div>
     );
   };
+
+  const ConfirmationMessage = () => {
+    return (
+      <div className="confirmation">
+        <div className="confirmation-message">
+          <h3 className="confirmation-message__title">Confirmation Message</h3>
+          <p className="confirmaton-message__description">
+            By Clicking "yes" this Ingredient will be added to the list
+          </p>
+          <button>Yes</button>
+        </div>
+      </div>
+    );
+  };
+
   return (
     <div>
       {ReactDOM.createPortal(
         <BackDrop onClick={props.onClick} />,
         document.getElementById("backdrop-root")
+      )}
+      {ReactDOM.createPortal(
+        <ConfirmationMessage onClick={props.onClick} />,
+        document.getElementById("modal-root")
       )}
     </div>
   );
